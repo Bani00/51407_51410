73070eda68e1210adcc21bb78998a3bff37cc831
Adam <adam.fiedko@gmail.com>
2024-05-26 22:52:36 +0200

    feat: add working button with currency comparison

--- frontend/src/components/Body/body.tsx
+++ frontend/src/components/Body/body.tsx
@@ -13,6 +13,11 @@ export function Body() {
   const [showExchangeRate, setShowExchangeRate] = useState(false);
   const [rates, setRates] = useState<ExchangeRate[]>([])
   const [submit, setSubmit] = useState(false)
+  const [firstCurrency, setFirstCurrency] = useState('')
+  const [secondCurrency, setSecondCurrency] = useState('')
+  const [showCurrencyComparison, setCurrencyComparison] = useState(false)
+  const [compare, setCompare] = useState(false)
+  const [compareValue, setCompareValue] = useState()
 
 
   useEffect(() => {
@@ -28,6 +33,12 @@ export function Body() {
       .then((data) => setRates(data.exchangeRates))
   }, [chosenCurrency]);
 
+  useEffect(() => {
+    fetch(`/api/currency/${firstCurrency}?compare_to=${secondCurrency}`)
+      .then((res) => res.json())
+      .then((data) => setCompareValue(data.exchangeRate))
+  }, [firstCurrency, secondCurrency]);
+
   const handleShowAllCurrencies = () => {
     setShowCurrencies(!showCurrencies);
   };
@@ -38,18 +49,24 @@ export function Body() {
   };
 
   const handleShowExchangeRateBetweenTwoCurrencies = () => {
-    // Logic to show exchange rate between two currencies
-    console.log('Show exchange rate between two currencies');
+    setCurrencyComparison(!showCurrencyComparison)
+    setCompare(false)
   };
 
   const handleSubmitButton = () => {
     setSubmit(!submit)
   }
 
+  const handleCompareButton = () => {
+    setCompare(!compare)
+  }
+
+
   return (
     <div className="body-container">
       <h1 className="title">Welcome to RateXchange</h1>
       <div className="buttons">
+        {/* Button - Show all available currencies */}
         <div className="allCurrencies-container">
           <button onClick={handleShowAllCurrencies}>
             {showCurrencies ? 'Hide all available currencies' : 'Show all available currencies'}
@@ -63,19 +80,20 @@ export function Body() {
             </div>
           </div>)}
         </div>
+        {/* Button show exchange rates */}
         <div className="exchangeRate-container">
-        <button onClick={handleShowOneCurrencyRate}>
-          {showExchangeRate ? 'Hide exchange rates' : 'Check exchange rates'}
-        </button>
-        {showExchangeRate && (
+          <button onClick={handleShowOneCurrencyRate}>
+            {showExchangeRate ? 'Hide exchange rates' : 'Check exchange rates'}
+          </button>
+          {showExchangeRate && (
           <div className="exchangeRates">
             <h2 className="exchangeRate-text">Choose one currency:</h2>
-            <select className="exchangeRate-selector" onChange={(e) => setCurrency(e.target.value)}>
-              <option>Available currencies:</option>
+            <select className="exchangeRate-selector" defaultValue="" onChange={(e) => setCurrency(e.target.value)}>
+              <option disabled={true} value="">Available currencies:</option>
               {currencies.map((elem, index) =>(
               <option key={index}>{elem}</option>))}
             </select>
-            <div>
+            <>
               <button className="submitButton" onClick={handleSubmitButton}>
                 {submit ? 'Hide' : 'Submit'}
               </button>
@@ -84,15 +102,43 @@ export function Body() {
                 {rates.map((item, index) => (
                 <li key={index} className="exchangeRate-listElement">
                   {item.currency}: {item.exchangeRate}
-                </li>
+                 </li>
                 ))}
-              </ul>
-              }
-            </div>
-          </div>
-        )}
+              </ul>}
+            </>
+          </div>)}
         </div>
-        <button onClick={handleShowExchangeRateBetweenTwoCurrencies}>Show exchange rate between two currencies</button>
+        {/* Button show currency comparison */}
+        <div>
+          <button onClick={handleShowExchangeRateBetweenTwoCurrencies}>
+            {showCurrencyComparison ? 'Hide comparison' : 'Show ratio between 2 currencies'}
+          </button>
+          {showCurrencyComparison && (
+            <div className="exchangeRates">
+            <h2 className="exchangeRate-text">Choose first currency:</h2>
+            <select className="exchangeRate-selector" defaultValue="" onChange={(e) => setFirstCurrency(e.target.value)}>
+            <option disabled={true} value="">Available currencies:</option>
+              {currencies.map((elem, index) => (
+              <option key={index}>{elem}</option>))}
+            </select>
+            <h2 className="exchangeRate-text">Choose second currency:</h2>
+            <select className="exchangeRate-selector" defaultValue="" onChange={(e) => setSecondCurrency(e.target.value)}>
+            <option disabled={true} value="">Available currencies:</option>
+              {currencies.map((elem, index) =>(
+              <option key={index}>{elem}</option>))}
+            </select>
+            <>
+              <button className="compareButton" onClick={handleCompareButton}>
+                {compare ? 'Hide' : 'Compare'}
+              </button>
+              {compare && 
+              <div className="currency-ratio">
+                <span className="bold">{firstCurrency}</span>: {compareValue} {secondCurrency}
+              </div>}
+            </>
+            </div>)}
+        </div>
+        
       </div>
     </div>
   );
